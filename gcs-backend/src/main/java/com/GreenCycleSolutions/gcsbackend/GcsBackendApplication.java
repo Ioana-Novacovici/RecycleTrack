1393aafea10eb5d928d42c91eaa9de0b28860dd7
unknown <ioananovacovici@gmail.com>
2024-02-24 18:14:34 +0200

    Initial project structure with Spring and React

---
+++ gcs-backend/src/main/java/com/GreenCycleSolutions/gcsbackend/GcsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GreenCycleSolutions.gcsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcsBackendApplication.class, args);
+	}
+
+}
---
+++ gcs-backend/src/test/java/com/GreenCycleSolutions/gcsbackend/GcsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreenCycleSolutions.gcsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
