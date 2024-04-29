7ce72201bbae484f7599b3d0cbf79b3992357aea
Ioana-Novacovici <ioananovacovici@gmail.com>
2024-04-29 17:30:26 +0300

    partially working login with pop up for error, the previous state is somehow kept

---
+++ gcs-frontend/green-cycle-solutions/src/components/error-modal/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function ErrorModal({ errorTitle, errorMessage }) {
+  return (
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex="-1"
+      aria-labelledby="modalTitle"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="modalTitle">
+              {errorTitle}
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body text-danger fst-italic fw-bold">
+            {errorMessage}
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorModal;
