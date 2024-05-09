404c6a1268444460bf25cc39e56afe3f942017f3
Ioana-Novacovici <ioananovacovici@gmail.com>
2024-05-09 14:02:07 +0300

    package renaming, remove unused imports

--- gcs-backend/src/main/java/com/GreenCycleSolutions/gcsbackend/dto/PasswordRenewDTO.java
+++ gcs-backend/src/main/java/com/GreenCycleSolutions/gcsbackend/dto/PasswordRenewDTO.java
@@ -1,6 +1,5 @@
 package com.GreenCycleSolutions.gcsbackend.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
