1e0e5b8e69bdb7035dcecc04bde26d992725e364
Ioana-Novacovici <ioananovacovici@gmail.com>
2024-05-15 17:52:18 +0300

    home initial version done

--- gcs-frontend/green-cycle-solutions/src/pages/home/index.jsx
+++ gcs-frontend/green-cycle-solutions/src/pages/home/index.jsx
@@ -1,17 +1,31 @@
 import { useContext } from "react";
 import AuthContext from "../../api/AuthProvider";
+import HomeImage from "../../assets/images/HomePageBg.jpg";
+import "../home/style.css";
 
 function Home() {
   const { auth } = useContext(AuthContext);
 
   return (
     <div>
-      Here is Home
-      {auth.usernameContext ? (
-        <p>
-          Hello {auth.usernameContext} {auth.role}
-        </p>
-      ) : null}
+      <div
+        className="card home-card mt-1"
+        style={{ color: "#354a3f", backgroundColor: "#dedccb" }}
+      >
+        <img src={HomeImage} className="card-img" alt="GCS" />
+        <div className="card-img-overlay">
+          <h5 className="fs-2 card-title ">
+            Reduce, Reuse, Recycle - Join our mission to a Greener Planet!
+          </h5>
+          <h5 className="card-text fs-5 fst-italic fw-normal">
+            Green-Cycle-Solutions goal is to make our planet greener through
+            selective waste collection and weekly recycling. <br />
+            Join us to easily recycle, track your collections and contribute to
+            a sustainable future because together, we can make a difference!
+          </h5>
+          <p className="card-text"></p>
+        </div>
+      </div>
     </div>
   );
 }
