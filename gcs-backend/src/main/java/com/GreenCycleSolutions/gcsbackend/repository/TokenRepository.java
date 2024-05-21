6ca35110387d0e764708fc7959fa1ea7e225a256
Ioana-Novacovici <ioananovacovici@gmail.com>
2024-05-21 20:53:48 +0300

    add missing repository annotation

--- gcs-backend/src/main/java/com/GreenCycleSolutions/gcsbackend/repository/TokenRepository.java
+++ gcs-backend/src/main/java/com/GreenCycleSolutions/gcsbackend/repository/TokenRepository.java
@@ -2,10 +2,12 @@ package com.GreenCycleSolutions.gcsbackend.repository;
 
 import com.GreenCycleSolutions.gcsbackend.entity.TokenEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TokenRepository extends JpaRepository<TokenEntity, Integer> {
 
     List<TokenEntity> findAllByUserIdAndExpiredFalse(Integer user_id);
