6eea6f5c5f8bb9b73a3dcff68e15d5fe3f3b4c09
Ioana-Novacovici <ioananovacovici@gmail.com>
2024-06-09 12:32:41 +0300

    block adding address for agents or admins

--- gcs-backend/src/main/java/com/GreenCycleSolutions/gcsbackend/service/AddressService.java
+++ gcs-backend/src/main/java/com/GreenCycleSolutions/gcsbackend/service/AddressService.java
@@ -5,6 +5,8 @@ import com.GreenCycleSolutions.gcsbackend.converter.AddressConverter;
 import com.GreenCycleSolutions.gcsbackend.dto.AddressDTO;
 import com.GreenCycleSolutions.gcsbackend.entity.AddressEntity;
 import com.GreenCycleSolutions.gcsbackend.entity.UserEntity;
+import com.GreenCycleSolutions.gcsbackend.entity.enumtype.Role;
+import com.GreenCycleSolutions.gcsbackend.exception.NotSupportedOperation;
 import com.GreenCycleSolutions.gcsbackend.exception.ResourceNotFoundException;
 import com.GreenCycleSolutions.gcsbackend.repository.AddressRepository;
 import com.GreenCycleSolutions.gcsbackend.repository.UserRepository;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -57,8 +60,13 @@ public class AddressService {
     public void addAddress(AddressDTO addressDTO) {
         var user = userRepository.findById(addressDTO.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("The user with id " + addressDTO.getId() + " does not exist"));
-        //generate the unique code for this address
-        addressDTO.setCollectionCode(UUID.randomUUID());
-        addressRepository.save(AddressConverter.convertToAddressEntity(addressDTO, user));
+        if(Objects.equals(user.getRole(), Role.USER)) {
+            //generate the unique code for this address
+            addressDTO.setCollectionCode(UUID.randomUUID());
+            addressRepository.save(AddressConverter.convertToAddressEntity(addressDTO, user));
+        } else {
+            throw new NotSupportedOperation("Can not link address to other roles beside USER yet.");
+        }
+
     }
 }
